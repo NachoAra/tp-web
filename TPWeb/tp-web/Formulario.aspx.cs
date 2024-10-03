08cb28977f59df01451c381e07f71c5fa281c531
Facundo Avalos <facundotaraujo@hotmail.com>
2024-10-03 19:59:13 -0300

    Método para registrar un cliente al enviar

--- TPWeb/tp-web/Formulario.aspx.cs
+++ TPWeb/tp-web/Formulario.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using dominio;
 using negocio;
 
@@ -15,18 +16,52 @@ namespace tp_web
         {
             ClienteNegocio clienteNegocio = new ClienteNegocio();
             int dni = int.Parse(DNIUsuario.Text);
-            Cliente cliente = clienteNegocio.ObtenerDatos(dni);
+            Cliente cliente = clienteNegocio.GetCliente(dni);
 
-            if (cliente != null)
+            try
             {
-                NombreUsuario.Text = cliente.Nombre;
-                ApellidoUsuario.Text = cliente.Apellido;
-                EmailUsuario.Text = cliente.Email;
-                DireccionUsuario.Text = cliente.Direccion;
-                CiudadUsuario.Text = cliente.Ciudad;
-                CPUsuario.Text = cliente.CodigoPostal.ToString();
+                if (cliente != null)
+                {
+                    NombreUsuario.Text = cliente.Nombre;
+                    ApellidoUsuario.Text = cliente.Apellido;
+                    EmailUsuario.Text = cliente.Email;
+                    DireccionUsuario.Text = cliente.Direccion;
+                    CiudadUsuario.Text = cliente.Ciudad;
+                    CPUsuario.Text = cliente.CodigoPostal.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
 
+        }
+
+        protected void BtnEnviarForm_Click(object sender, EventArgs e)
+        {
+            ClienteNegocio clienteNegocio = new ClienteNegocio();
+            Cliente cliente = new Cliente();
+
+            try
+            {
+                cliente.Documento = DNIUsuario.Text;
+                cliente.Nombre = NombreUsuario.Text;
+                cliente.Apellido = ApellidoUsuario.Text;
+                cliente.Email = EmailUsuario.Text;
+                cliente.Direccion = DireccionUsuario.Text;
+                cliente.Ciudad = CiudadUsuario.Text;
+                cliente.CodigoPostal = int.Parse(CPUsuario.Text);
+
+                // Validar campos antes de insertar
+
+                clienteNegocio.RegistrarCliente(cliente);
+
+                // Codigo que evalue si RegitrarCliente es true o false e indicar mediante un cartel al usuario.
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
