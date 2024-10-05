6c205e9f1c7028ba76c09178b3a85db397df23a4
Facundo Avalos <facundotaraujo@hotmail.com>
2024-10-05 20:50:00 -0300

    Nuevas propiedas del form

--- TPWeb/tp-web/Formulario.aspx.designer.cs
+++ TPWeb/tp-web/Formulario.aspx.designer.cs
@@ -15,6 +15,24 @@ namespace tp_web
     {
 
         /// <summary>
+        /// CardRegistroExitoso control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl CardRegistroExitoso;
+
+        /// <summary>
+        /// TiempoEspera control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.Timer TiempoEspera;
+
+        /// <summary>
         /// DNIUsuario control.
         /// </summary>
         /// <remarks>
@@ -211,23 +229,5 @@ namespace tp_web
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button BtnEnviarForm;
-
-        /// <summary>
-        /// CardRegistroExitoso control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl CardRegistroExitoso;
-
-        /// <summary>
-        /// TiempoEspera control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.Timer TiempoEspera;
     }
 }
